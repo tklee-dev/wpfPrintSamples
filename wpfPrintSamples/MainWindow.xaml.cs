a40c8ac6cda85cbeb4a669bc184bf2dad07cc0b8
TaeKyungLee <tklee.me@gmail.com>
2021-04-26 15:24:58 +0900

    Autofit, Auto Rotate화질 저하없이 개발완료

--- wpfPrintSamples/MainWindow.xaml.cs
+++ wpfPrintSamples/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace wpfPrintSamples
             InitializeComponent();
         }
         string imgPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "sample_city.jpg");
-        string testSavePath1 = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "1140537.jpg");
+        string testSavePath1 = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "sample_test.png");
         string testSavePath2 = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "sample_city_test2.jpg");
         string printerDescription = "samplePic";
 
@@ -50,48 +50,13 @@ namespace wpfPrintSamples
             bi.BeginInit();
             bi.CacheOption = BitmapCacheOption.OnLoad;
 
-            bi.UriSource = new Uri(testSavePath1);
+            bi.UriSource = new Uri(imgPath);
             bi.EndInit();
 
             return bi;
         }
 
-        private void Fit_Auto_FE(Visual v)
-        {
-            //https://stackoverflow.com/questions/7931961/wpf-printing-to-fit-page , seanzi
-
-            System.Windows.FrameworkElement fe = v as System.Windows.FrameworkElement;
-            if (fe == null)
-                return;
-
-            PrintDialog pd = new PrintDialog();
-            if (pd.ShowDialog() == true)
-            {
-                //store original scale
-                Transform originalScale = fe.LayoutTransform;
-                //get selected printer capabilities
-                System.Printing.PrintCapabilities capabilities = pd.PrintQueue.GetPrintCapabilities(pd.PrintTicket);
-
-                //get scale of the print wrt to screen of WPF visual
-                double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / fe.ActualWidth, capabilities.PageImageableArea.ExtentHeight /fe.ActualHeight);
-
-                //Transform the Visual to scale
-                fe.LayoutTransform = new ScaleTransform(scale, scale);
-
-                //get the size of the printer page (일반적으로 UI가 더작기때문에 MIN임)
-                System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
-
-                //update the layout of the visual to the printer page size.
-                fe.Measure(sz);
-                fe.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
-
-                //now print the visual to printer to fit on the one page.
-                pd.PrintVisual(v, "SamplePic");
-
-                //apply the original transform. FrameworkElement(UI)를 바로 사용하는 경우도 있기때문에.
-                fe.LayoutTransform = originalScale;
-            }
-        }
+        
 
         private BitmapSource Fit_Auto(BitmapSource bs)
         {
@@ -180,10 +145,63 @@ namespace wpfPrintSamples
         //AutoRotate
         private void button4_Click(object sender, RoutedEventArgs e)
         {
+
+
+            //비트맵 크기자체를 영역에 맞추기떄문에 화질저하있음
             BitmapSource bi = ImageLoad();
             bi = Rotate_Auto(bi);
             bi = Fit_Auto(bi);
             Print_NonDialog(bi);
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bi = ImageLoad();
+            bi = Rotate_Auto(bi);
+            Image img = new Image();
+            img.Source = bi;
+            Visual vi = img;
+            Fit_Auto_FE(vi);
+
+        }
+
+
+        private void Fit_Auto_FE(Visual v)
+        {
+            //https://stackoverflow.com/questions/7931961/wpf-printing-to-fit-page , seanzi
+
+            System.Windows.FrameworkElement fe = v as System.Windows.FrameworkElement;
+            if (fe == null)
+                return;
+
+            PrintDialog pd = new PrintDialog();
+            if (pd.ShowDialog() == true)
+            {
+                //store original scale
+                Transform originalScale = fe.LayoutTransform;
+                //get selected printer capabilities
+                System.Printing.PrintCapabilities capabilities = pd.PrintQueue.GetPrintCapabilities(pd.PrintTicket);
+
+                //get scale of the print wrt to screen of WPF visual
+                double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / fe.ActualWidth, capabilities.PageImageableArea.ExtentHeight / fe.ActualHeight);
+
+                
+                //Transform the Visual to scale
+                fe.LayoutTransform = new ScaleTransform(1, 1);
+
+                //get the size of the printer page (일반적으로 UI가 더작기때문에 MIN임)
+                System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+
+                //update the layout of the visual to the printer page size.
+                fe.Measure(sz);
+                fe.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
+
+                //now print the visual to printer to fit on the one page.
+                pd.PrintVisual(v, "SamplePic");
+
+                //apply the original transform. FrameworkElement(UI)를 바로 사용하는 경우도 있기때문에.
+                fe.LayoutTransform = originalScale;
+            }
         }
     }
 }
